8aabf2b6324adef5372980d969a7ecd96020cdb5
Lincolin <lincolinsabatia@gmail.com>
2024-09-15 10:32:44 +0300

    Initial Commit

---
+++ src/main/java/com/text/text_editor/TextEditorApplication.java
@@ -0,0 +1,13 @@
+package com.text.text_editor;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TextEditorApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(TextEditorApplication.class, args);
+	}
+
+}
